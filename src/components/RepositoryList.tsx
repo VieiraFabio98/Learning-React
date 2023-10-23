a05a81f17c90bb047f01a17d5653a50ec9927298
FabioAlvesVA <fabio.alves@vamilly.com.br>
2023-10-23 00:35:08 -0300

    Arrumando a tipagem.

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,9 +4,15 @@ import { useState, useEffect } from 'react'
 import '../styles/repositories.scss'
 
 
+interface IRepository {
+  name: string
+  description: string
+  html_url: string
+}
+
 export function RepositoryList() {
 
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<IRepository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
